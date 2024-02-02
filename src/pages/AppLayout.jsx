8925efbcd0fdd4ace01138b0ca34fb082718fef0
Stas Masevych <stasmasevych@gmail.com>
2024-02-03 00:46:29 +0100

    adding AppLayout

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,10 +1,12 @@
-import PageBar from "../components/PageNav";
+import Map from "../components/Map";
+import Sidebar from "../components/Sidebar";
 
+import styles from "./AppLayout.module.css";
 function AppLayout() {
   return (
-    <div>
-      <PageBar />
-      <p>App</p>
+    <div className={styles.app}>
+      <Sidebar />
+      <Map />
     </div>
   );
 }
