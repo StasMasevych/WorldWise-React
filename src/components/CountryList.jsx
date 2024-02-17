1329ed7caaded1c5c79c355b6911b7ca744148ac
Stas Masevych <stasmasevych@gmail.com>
2024-02-18 00:41:02 +0100

    adding unique countries

--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -14,15 +14,40 @@ function CountryList({ cities, isLoading }) {
   // Extract unique countries
   const uniqueCountries = [...new Set(cities.map((city) => city.country))];
 
-  console.log(uniqueCountries);
+  // Create arr of objects  with country and emoji properties
+  const countries = uniqueCountries.map((country) => {
+    //take the objects to put in the new arr
+    const cityWithCountry = cities.find((city) => city.country === country);
+
+    return {
+      country: country,
+      emoji: cityWithCountry.emoji,
+    };
+  });
+
+  /* console.log(uniqueCountries); */
+  /*  console.log(countries); */
+
+  // other option with reduce
+
+  const countries2 = cities.reduce((arr, city) => {
+    //if arr does not have country then new arr and push countries
+
+    if (!arr.map((el) => el.city).includes(city.country)) {
+      return [...arr, { country: city.country, emoji: city.emoji }];
+    } else {
+      return arr;
+    }
+  }, []);
+
+  console.log(countries2);
 
   return (
-    <div></div>
-    /*  <ul className={styles.countryList}>
-      {countries.map((country) => (
+    <ul className={styles.countryList}>
+      {countries2.map((country) => (
         <CountryItem key={country.id} country={country} />
       ))}
-    </ul> */
+    </ul>
   );
 }
 
