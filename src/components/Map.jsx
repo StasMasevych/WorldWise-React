e7749e508d75d73f9d657555d136d73308afb7fb
Stas Masevych <stasmasevych@gmail.com>
2024-03-21 17:38:22 +0100

    adding styles to leaflet components

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -3,11 +3,13 @@ import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
 import styles from "./Map.module.css";
 import { useState } from "react";
+import { useCities } from "../context/CitiesContext";
 
 function Map() {
   const [searchParams, setSearchParams] = useSearchParams();
   const navigate = useNavigate();
   const [mapPosition, setMapPosition] = useState([40, 0]);
+  const { cities } = useCities();
 
   const lat = searchParams.get("lat");
   const lng = searchParams.get("lng");
@@ -24,11 +26,19 @@ function Map() {
           url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         />
-        <Marker position={mapPosition}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+        {cities.map((city) => {
+          return (
+            <Marker
+              position={[city.position.lat, city.position.lng]}
+              key={city.id}
+            >
+              <Popup>
+                <span>{city.emoji}</span>
+                <span>{city.cityName}</span>
+              </Popup>
+            </Marker>
+          );
+        })}
       </MapContainer>
     </div>
   );
