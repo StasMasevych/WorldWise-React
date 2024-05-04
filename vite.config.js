920f206d0f13438aa390609ea994260b945cbed5
Stas Masevych <stasmasevych@gmail.com>
2024-05-04 16:05:11 +0200

    changing vite config

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,13 @@ export default defineConfig({
   plugins: [react()],
   build: {
     assetsInlineLimit: 0, // Ensure that all assets are included
+    chunkSizeWarningLimit: 1000, // Adjust the chunk size warning limit as needed
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // You can define manual chunks here if needed
+        },
+      },
+    },
   },
 });
