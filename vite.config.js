6a73a08efe6c7841f52724f4892889bf3a572967
Stas Masevych <stasmasevych@gmail.com>
2024-05-04 16:24:32 +0200

    new upd vite config

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    assetsInlineLimit: 0, // Ensure that all assets are included
-    chunkSizeWarningLimit: 1000, // Adjust the chunk size warning limit as needed
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          // You can define manual chunks here if needed
-        },
-      },
-    },
-  },
 });
