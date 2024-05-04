aa06c5e1195ac776d31c96b40e801877cdb49593
Stas Masevych <stasmasevych@gmail.com>
2024-05-04 15:49:25 +0200

    adding config for images to vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    assetsInlineLimit: 0, // Ensure that all assets are included
+  },
+});
